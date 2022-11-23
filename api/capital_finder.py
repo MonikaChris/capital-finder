82a9b6f8deeb237a1024b2a77a6a055388b8166b
Monika <monidavies@gmail.com>
2022-11-23 15:50:38 -0800

    Gets capital

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,12 +1,27 @@
 from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from urllib import parse
+import requests
 
 
 class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        s = self.path
+        url_components = parse.urlsplit(s)
+        query_string_list = parse.parse_qsl(url_components.query)
+        print(query_string_list)
+        dic = dict(query_string_list)
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
+        country = dic["country"]
+
+        url = "https://restcountries.com/v2/name/"
+        r = requests.get(url + dic["country"])
+        data = r.json()
+        print(data)
+        capital = data[0]['capital']
+        message = f'The capital of {dic["country"]} is {capital}.'
+
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(message.encode())
+        return
