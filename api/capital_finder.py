557d3e09a49cabae462fbc07425fbe90a9d1b5d5
Monika <monidavies@gmail.com>
2022-11-23 16:07:38 -0800

    Adds search by capital

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -8,17 +8,21 @@ class handler(BaseHTTPRequestHandler):
         s = self.path
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
-        print(query_string_list)
         dic = dict(query_string_list)
 
-        country = dic["country"]
+        if "country" in dic:
+            url = "https://restcountries.com/v2/name/"
+            r = requests.get(url + dic["country"])
+            data = r.json()
+            capital = data[0]['capital']
+            message = f'The capital of {dic["country"]} is {capital}.'
 
-        url = "https://restcountries.com/v2/name/"
-        r = requests.get(url + dic["country"])
-        data = r.json()
-        print(data)
-        capital = data[0]['capital']
-        message = f'The capital of {dic["country"]} is {capital}.'
+        if "capital" in dic:
+            url = "https://restcountries.com/v2/capital/"
+            r = requests.get(url + dic["capital"])
+            data = r.json()
+            country = data[0]['name']
+            message = f'{dic["capital"]} is the capital of {country}.'
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
